f7fd0a3bff1a7c64d284883ecbfd7667c52defa2
Kirtan <kirtan2202@yahoo.com>
2024-01-16 03:19:28 -0500

    updated

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ while game_is_on:
         #     pass
         if snake.head.distance(segment) < 10:
             game_is_on = False
-            scoreboard.game_over() 
-    
+            scoreboard.game_over()
+
 
 
 screen.exitonclick()
